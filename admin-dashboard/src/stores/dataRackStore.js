bdc1c4076a0f4d45321af1e235a7123eee34ff2f
Rene Christensen <rene723@gmail.com>
2024-04-17 13:38:08 +0200

    Fixed state issues

--- admin-dashboard/src/stores/dataRackStore.js
+++ admin-dashboard/src/stores/dataRackStore.js
@@ -2,14 +2,16 @@ import { defineStore } from 'pinia';
 
 export const useDataRackStore = defineStore('dataRack', {
   state: () => ({
-    currentDataRackId: null
+    currentDataRackId: localStorage.getItem('currentDataRackId')
   }),
   actions: {
     setDataRackId(id) {
       this.currentDataRackId = id;
+      localStorage.setItem('currentDataRackId', id);
     },
     clearDataRackId() {
       this.currentDataRackId = null;
+      localStorage.removeItem('currentDataRackId');
     }
   }
 });
